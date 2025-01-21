bb3b2257143104827b9e6e8f4508438a52cb854a
ozanArslan2424 <136094471+ozanArslan2424@users.noreply.github.com>
2025-01-21 19:05:41 +0300

    db migrate

---
+++ db/drizzle-out/0001_keen_adam_destine.sql
@@ -0,0 +1,17 @@
+PRAGMA foreign_keys=OFF;--> statement-breakpoint
+CREATE TABLE `__new_user` (
+	`id` text PRIMARY KEY NOT NULL,
+	`name` text NOT NULL,
+	`email` text NOT NULL,
+	`image` text,
+	`about` text,
+	`emailVerified` integer NOT NULL,
+	`createdAt` integer NOT NULL,
+	`updatedAt` integer NOT NULL
+);
+--> statement-breakpoint
+INSERT INTO `__new_user`("id", "name", "email", "image", "about", "emailVerified", "createdAt", "updatedAt") SELECT "id", "name", "email", "image", "about", "emailVerified", "createdAt", "updatedAt" FROM `user`;--> statement-breakpoint
+DROP TABLE `user`;--> statement-breakpoint
+ALTER TABLE `__new_user` RENAME TO `user`;--> statement-breakpoint
+PRAGMA foreign_keys=ON;--> statement-breakpoint
+CREATE UNIQUE INDEX `user_email_unique` ON `user` (`email`);
--- db/migrate.ts
+++ db/migrate.ts
@@ -1,7 +1,6 @@
 import { migrate } from "drizzle-orm/bun-sqlite/migrator";
-import config from "@/app.config";
 import { log } from "@/server/lib/log";
 import { db } from ".";
 
-migrate(db, { migrationsFolder: config.db.drizzleOutFolder });
+migrate(db, { migrationsFolder: "./drizzle-out" });
 log.success("migrate.ts ran successfully");
